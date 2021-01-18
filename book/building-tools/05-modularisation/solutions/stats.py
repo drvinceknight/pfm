852c2cb593052c2c2170751624b0097faf068d72
Vince Knight <vincent.knight@gmail.com>
2021-01-18 14:22:54 +0000

    Write solutions to modularisation (#158)
    * Write solutions to modularisation chapter.
    
    * Fix minor style check errors.
    
    * Add import of setuptools.
    
    This is not necessary to run any code (input is removed) but it is
    required to stop a specific warning.
    
    * Move the numpy imports.

---
+++ book/building-tools/05-modularisation/solutions/stats.py
@@ -0,0 +1,16 @@
+def get_mean(iterable):
+    """
+    Returns the mean of a given iterable which is defined as the sum divided by
+    the number of items.
+    """
+    return sum(iterable) / len(iterable)
+
+
+def get_population_variance(iterable):
+    """
+    Returns the population variance of a given iterable which is defined as the
+    mean square of the differences from the mean.
+    """
+    mean = get_mean(iterable)
+    squares_of_differences = [(value - mean) ** 2 for value in iterable]
+    return get_mean(squares_of_differences)
